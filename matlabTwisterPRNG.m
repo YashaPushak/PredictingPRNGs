786628ac43f0ad9ec1b7ea6d18d05198530c09ee
Kimberly Dextras-Romagnino <k.dextras.romagnino@gmail.com>
2016-04-11 11:48:51 -0700

    adding counting features

--- matlabTwisterPRNG.m
+++ matlabTwisterPRNG.m
@@ -1,4 +1,4 @@
-function [X,y,Xtest,Ytest] = matlabTwisterPRNG(n,d,t,k)
+function [X,y,Xtest,Ytest] = matlabTwisterPRNG(n,d,t,k, featureType)
 %n - number of inputs
 %d - Number of preceeding values used for predictions
 %t - Number of test values
@@ -9,9 +9,15 @@ function [X,y,Xtest,Ytest] = matlabTwisterPRNG(n,d,t,k)
 %the PRNG
 % rng(0,'twister');
 % rng('twister');
-
 numsTrain = randi(k,1,n+d);
-[X,y] = reformat(numsTrain,n,d,k);
 numsTest = randi(k,1,t+d);
-[Xtest,Ytest] = reformat(numsTest,t,d,k);
+if ( featureType == 's')
+    
+    [X,y] = reformat(numsTrain,n,d,k);
+    [Xtest,Ytest] = reformat(numsTest,t,d,k);
+else
+if ( featureType == 'c')
+    [X,y] = reformatCounting(numsTrain,n,d,k);
+    [Xtest,Ytest] = reformatCounting(numsTest,t,d,k); 
+end
 end
