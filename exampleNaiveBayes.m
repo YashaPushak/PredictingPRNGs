6cf318fc1ef215626b6f49d6cbe8b3995d427aac
Kimberly Dextras-Romagnino <k.dextras.romagnino@gmail.com>
2016-04-12 20:35:25 -0700

    small refactors

--- exampleNaiveBayes.m
+++ exampleNaiveBayes.m
@@ -2,19 +2,17 @@ clear
 
 for i = 1
     
-     n = 1000;
+    n = 4000;
     v = 1000;
-    t = 1000;
-    d = 12;
-    k = 5;
+    t = 4000;
+    d = 9;
+    k = 2;
     featureType = 's';
     labelSize = 1;
     seed= 0;
     PRNGtype = 'yasha';
     
     [X,y,Xval, Yval, Xtest,ytest] = PRNGs(PRNGtype, n, v, t, d, k, featureType, labelSize, seed);
-  
-    
     
     
     if (featureType =='s')
@@ -27,24 +25,14 @@ for i = 1
         end
     end
     
-    
-    
-    
     model = naiveBayes(X,y);
     yhat = model.predict(model,Xtest);
     err(i) = sum(yhat ~= ytest)/t;
-
-    
-     fprintf('Best Error for Naive Bayes: %.04f \n', minErr);
-    fprintf('Best D for Naive Bayes: %.04f \n', minD);
     
     disp(['iteration: ' num2str(i) ]);
     fprintf('Average Error for Naive Bayes: %.04f \n', mean(err));
     fprintf('Median Error for Naive Bayes: %.04f \n', median(err));
-    
-    
-    
-    
+
     if(mod(i,100) == 0)
         cdfplot(err);
         pause(0.1);
